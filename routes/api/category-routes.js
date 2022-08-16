b7ec3d0e0c07e2de833c0e65132ce3cf006a88b6
Camila Zagal <camilazags@gmail.com>
2022-08-16 13:21:55 -0500

    updated category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,78 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoriesDB = await Category.findAll({
+      include: [{model: Product}],
+    });
+    res.status(200).json(categoriesDB);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoriesDB = await Category.findByPk(re.params.id, {
+      include: [{model: Product}],
+    });
+    if(!categoriesDB) {
+      res.status(404).json({message: "Couldn't find category."});
+      return;
+    }
+    res.status(200).json(categoriesDB);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  try {
+    const categoriesDB = await Category.create(req.body);
+    res.status(200).json({
+      message: `New category with the name: ${categoriesDB.category_name} was created.`
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((newCategory) => {
+    res.json(newCategory);
+  })
+  .catch((err) => {
+    res.json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const categoriesDB = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoriesDB) {
+      res.status(404).json({
+        message: 'No category found.'
+      });
+      return;
+    }
+    res.status(200).json(categoriesDB);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
